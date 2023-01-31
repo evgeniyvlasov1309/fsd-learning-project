6509ffcc41e05f84aeb438c9ba243eb2182ad1ee
Evgeniy Vlasov <evgeniy.vlasov@rokolabs.com>
2023-01-31 22:35:20 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { classNames } from "./helpers/classNames/classNames";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
+
+export default function App() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback="Loading...">
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,5 @@
+import classes from "./Counter.module.scss";
+
+export default function Counter() {
+  return <div className={classes.counter}>counter</div>;
+}
