221e2846da321969a6572edd8e68118555a5cc1f
Evgeniy Vlasov <evgeniy.vlasov@rokolabs.com>
2023-09-15 12:07:15 +0200

    add new theme

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -26,6 +26,9 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
             loader: "babel-loader",
             options: {
                 presets: ["@babel/preset-env"],
+                plugins: [
+                    isDev && require.resolve('react-refresh/babel'),
+                ].filter(Boolean),
             },
         },
     };
--- src/app/providers/Theme/lib/useTheme.ts
+++ src/app/providers/Theme/lib/useTheme.ts
@@ -13,11 +13,24 @@ interface UseThemeResult {
 export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
-    function toggleTheme() {
-        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+    const toggleTheme = () => {
+        let newTheme: Theme;
+        switch (theme) {
+        case Theme.DARK:
+            newTheme = Theme.LIGHT;
+            break;
+        case Theme.LIGHT:
+            newTheme = Theme.ORANGE;
+            break;
+        case Theme.ORANGE:
+            newTheme = Theme.DARK;
+            break;
+        default:
+            newTheme = Theme.LIGHT;
+        }
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    }
+    };
 
     return { theme: theme || Theme.LIGHT, toggleTheme };
 }
